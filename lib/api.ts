376609af799fa714034b5574b8f56b2c813b0610
rullyfadheli <rullyfadheli@gmail.com>
2025-07-24 23:01:02 +0700

    Refactor API methods to include token handling and add Wishlist type definition

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,5 @@
 import { type Product, type ErrorMessage } from "@/types/product";
+import type { Token } from "@/types";
 
 class ProductAPI {
   async getProductById(id: string): Promise<Product | String> {
@@ -16,19 +17,90 @@ class ProductAPI {
     return result as Product;
   }
 
-  public async getWishList(user_id: string, token: string) {
-    const response = await fetch(
-      `http://localhost/3001/wishlist?user_id=${user_id}`,
-      {
-        method: "GET",
-        headers: {
-          authorization: token,
-        },
-      }
-    );
+  public async getWishList(token: string) {
+    console.log("token", token);
+    const response = await fetch(`http://localhost:3001/api/wishlist`, {
+      method: "GET",
+      headers: {
+        authorization: `Bearer ${token}`,
+      },
+    });
+    // console.log(response);
+    const data = await response.json();
+    // console.log(data);
+    if (!Array.isArray(data)) {
+      return false;
+    }
+
+    return data;
+  }
+
+  // Fetching refresh token from server
+  public async getRefreshToken(): Promise<Token | boolean> {
+    const response = await fetch("http://localhost:3001/api/token", {
+      method: "GET",
+      credentials: "include",
+    });
 
     const data = await response.json();
-    return data;
+    // console.log(data);
+
+    if (!Array.isArray(data)) {
+      return false;
+    }
+
+    return data as Token;
+  }
+
+  public async getBestDeals(): Promise<Product | boolean> {
+    const response = await fetch("http://localhost:3001/api/best-deal", {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      console.warn("API error:", data[0].message as string);
+      return false;
+    }
+
+    return data as Product;
+  }
+
+  public async getPopularProducts(): Promise<Product | boolean> {
+    const response = await fetch("http://localhost:3001/api/popular-products", {
+      method: "GET",
+    });
+
+    const data = await response.json();
+
+    if (response.status !== 200) {
+      console.warn("API error:", data[0].message as string);
+      return false;
+    }
+
+    return data as Product;
+  }
+
+  public async addToCart(
+    product_id: string,
+    token: string
+  ): Promise<string | boolean> {
+    const response = await fetch("http://localhost:3001/api/add-to-cart", {
+      method: "POST",
+      headers: {
+        authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ product_id }),
+    });
+
+    const data = await response.json();
+    if (response.status !== 201) {
+      return false;
+    }
+
+    return data[0].message as string;
   }
 }
 
